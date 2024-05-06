c9788d2819b609e523ac2f6f58f6737bf8d16610
mats-bb <mats.k.berg@gmail.com>
2024-05-06 12:36:02 +0200

    added about.txt

--- tournament_game_generator/test.py
+++
@@ -1,7 +0,0 @@
-my_list = [1, 2, 3, 4, 5, 6, 7, 8]
-
-# Calculate the number of pairs
-for i in range(len(my_list) // 2):
-        away = my_list[i]
-        home = my_list[-(i + 1)]
-        print(f"Home: {home} VS Away: {away}")
